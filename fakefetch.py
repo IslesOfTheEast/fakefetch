a88957773107f769f0c89935616ec39227005681
JackCDK <JackCDK@users.noreply.github.com>
2016-10-26 20:24:35 +0100

    added colorama in some places

--- fakefetch.py
+++ fakefetch.py
@@ -3,53 +3,41 @@
 #add your own info in. I'm not gonna add hardware/os dection but, you can.
 #all ascii art is from: http://chris.com/ascii/
 import argparse
+from colorama import *
 
 #ascii logos
 
 anchor = '''
-         _
-        (_)         [techgnome]
-         |          [OS]: Debian 8.6 jessie
-    ()---|---()     [Kernel]: x86_64 Linux 3.16.0-4-amd64
-         |          [WM]: OpenBox
-         |
-  __     |     __
- |\     /^\     /|
-   '..-'   '-..'
-     `-._ _.-`
-         `
-'''
-axe = '''
-     ,  /\  .       [techgnome]
-    //`-||-'\\      [OS]: Debian 8.6 jessie
-   (| -=||=- |)     [Kernel]: x86_64 Linux 3.16.0-4-amd64
-    \\,-||-.//      [WM]: OpenBox
-     `  ||  '
-        ||
-        ||
-        ||
-        ||
-        ||
-        ()
+        \033[37m _
+        \033[37m(_)         \033[31m[techgnome]
+         \033[37m|          \033[31m[OS]:\033[39m Debian 8.6 jessie
+    \033[37m()---|---()     \033[31m[Kernel]:\033[39m x86_64 Linux 3.16.0-4-amd64
+         \033[37m|          \033[31m[WM]:\033[39m OpenBox
+         \033[37m|
+ \033[37m __     |     __
+ \033[37m|\     /^\     /|
+   \033[37m'..-'   '-..'
+     \033[37m`-._ _.-`
+         \033[37m`
 '''
 
 linux = '''
-       a8888b.
-      d888888b.     [techgnome]
-      8P"YP"Y88     [OS]: Debian 8.6 jessie
-      8|o||o|88     [Kernel]: x86_64 Linux 3.16.0-4-amd64
-      8'    .88     [WM]: OpenBox
-      8`._.' Y8.
-     d/      `8b.
-    dP   .    Y8b.
-   d8:'  "  `::88b
-  d8"         'Y88b
- :8P    '      :888
-  8a.   :     _a88P
-._/"Yaa_:   .| 88P|
-\    YP"    `| 8P  `.
-/     \.___.d|    .'
-`--..__)8888P`._.'
+       \033[37ma8888b.
+      \033[37md888888b.     \033[31m[techgnome]
+      \033[37m8P"YP"Y88     \033[31m[OS]: \033[39mDebian 8.6 jessie
+      \033[37m8|o||o|88     \033[31m[Kernel]: \033[39mx86_64 Linux 3.16.0-4-amd64
+      \033[37m8'    .88     \033[31m[WM]: \033[39mOpenBox
+      \033[37m8`._.' Y8.
+     \033[37md/      `8b.
+    \033[37mdP   .    Y8b.
+   \033[37md8:'  "  `::88b
+  \033[37md8"         'Y88b
+ \033[37m:8P    '      :888
+  \033[37m8a.   :     _a88P
+\033[37m._/"Yaa_:   .| 88P|
+\033[37m\    YP"    `| 8P  `.
+\033[37m/     \.___.d|    .'
+\033[37m`--..__)8888P`._.'
 
 
 '''
@@ -59,15 +47,12 @@ linux = '''
 #arg stuff
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-l','--logo', choices=['anchor', 'axe', 'linux',], default='anchor', help='choose the ascii art you want')
+parser.add_argument('-l','--logo', choices=['anchor', 'linux',], default='anchor', help='This will let you turn your apache2 server on or off and you can even restart it')
 
 
 args = parser.parse_args()
 if args.logo == "anchor":
     print(anchor)
-
-elif args.logo == "axe":
-	print(axe)
 
 elif args.logo == "linux":
 	print(linux)
