be7049b1fcf0a0fbdc6407eb0da72d4008b6320a
JackCDK <JackCDK@users.noreply.github.com>
2016-10-27 22:11:47 +0100

    added ctest and more colors

--- fakefetch.py
+++ fakefetch.py
@@ -24,7 +24,7 @@ anchor = '''
     \033[37m()---|---()     \033[32m[Kernel]:\033[39m {}
          \033[37m|          \033[32m[WM]:\033[39m OpenBox
          \033[37m|
- \033[37m __     |     __
+ \033[37m __     |     __   \033[40m  \033[41m  \033[42m  \033[43m  \033[44m  \033[45m  \033[46m  \033[47m \033[48m \033[49m
  \033[37m|\     /^\     /|
    \033[37m'..-'   '-..'
      \033[37m`-._ _.-`
@@ -38,7 +38,7 @@ tux = '''
       \033[37m8|o||o|88     \033[32m[Kernel]: \033[39m {}
       \033[37m8'    .88     \033[32m[WM]:\033[39m OpenBox
       \033[37m8`._.' Y8.
-     \033[37md/      `8b.
+     \033[37md/      `8b.   \033[40m  \033[41m  \033[42m  \033[43m  \033[44m  \033[45m  \033[46m  \033[47m \033[48m \033[49m
     \033[37mdP   .    Y8b.
    \033[37md8:'  "  `::88b
   \033[37md8"         'Y88b
@@ -46,11 +46,23 @@ tux = '''
 '''.format(getpass.getuser(),platform.release())
 
 
+colortest = '''
+
+
+ \033[40m black \033[41m red \033[42m green \033[43m yellow \033[44m blue \033[45m purple \033[46m cyan \033[47m grey\033[48m \033[49m
+ \033[40m black \033[41m red \033[42m green \033[43m yellow \033[44m blue \033[45m purple \033[46m cyan \033[47m grey\033[48m \033[49m
+ \033[40m black \033[41m red \033[42m green \033[43m yellow \033[44m blue \033[45m purple \033[46m cyan \033[47m grey\033[48m \033[49m
+ \033[40m black \033[41m red \033[42m green \033[43m yellow \033[44m blue \033[45m purple \033[46m cyan \033[47m grey\033[48m \033[49m
+ \033[40m black \033[41m red \033[42m green \033[43m yellow \033[44m blue \033[45m purple \033[46m cyan \033[47m grey\033[48m \033[49m
+ \033[40m black \033[41m red \033[42m green \033[43m yellow \033[44m blue \033[45m purple \033[46m cyan \033[47m grey\033[48m \033[49m
+ \033[40m black \033[41m red \033[42m green \033[43m yellow \033[44m blue \033[45m purple \033[46m cyan \033[47m grey\033[48m \033[49m
+
+'''
 
 #arg stuff
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-l','--logo', choices=['anchor', 'tux',], default='anchor', help='choose ascii logo')
+parser.add_argument('-l','--logo', choices=['anchor', 'tux','ctest'], default='anchor', help='choose ascii logo')
 
 
 args = parser.parse_args()
@@ -59,3 +71,6 @@ if args.logo == "anchor":
 
 elif args.logo == "tux":
 	print(tux)
+
+elif args.logo == "ctest":
+	print(colortest)
