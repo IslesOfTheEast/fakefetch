f6792e370a4b229fcab50efa3a3e61f455a2c6be
JackCDK <JackCDK@users.noreply.github.com>
2016-10-27 08:46:42 +0100

    added the fetching of details and more colors
    I've added :
    username detection
    kernel detection
    
    and some more colors

--- fakefetch.py
+++ fakefetch.py
@@ -1,35 +1,42 @@
 #!/usr/bin/python
 #made by JackCDK
+#add your own info in. I'm not gonna add hardware/os dection but, you can.
 #all ascii art is from: http://chris.com/ascii/
-
-#as of 26/10/16 fakefetch.py does not have username, kernel, os or window manager detection. 
-#please add that info in by yourself for now
-
-import argparse
+import argparse, getpass, platform, os
 from colorama import *
+
+#color guide
+# "\033[37m"   white
+# "\033[33m"   black
+# "\033[31m"   red
+# "\033[32m"   green
+# "\033[33m"   yellow
+# "\033[34m"   blue
+# "\033[35m"   magenta
+# "\033[36m"   cyan
 
 #ascii logos
 
 anchor = '''
         \033[37m _
-        \033[37m(_)         \033[31m[techgnome]
-         \033[37m|          \033[31m[OS]:\033[39m Debian 8.6 jessie
-    \033[37m()---|---()     \033[31m[Kernel]:\033[39m x86_64 Linux 3.16.0-4-amd64
-         \033[37m|          \033[31m[WM]:\033[39m OpenBox
+        \033[37m(_)         \033[32m[{}]
+         \033[37m|          \033[32m[OS]:\033[39m Debian 8.6 jessie
+    \033[37m()---|---()     \033[32m[Kernel]:\033[39m {}
+         \033[37m|          \033[32m[WM]:\033[39m OpenBox
          \033[37m|
  \033[37m __     |     __
  \033[37m|\     /^\     /|
    \033[37m'..-'   '-..'
      \033[37m`-._ _.-`
-         \033[37m`
-'''
+         \033[37m`\033[39m
+'''.format(getpass.getuser(),platform.release())
 
 linux = '''
        \033[37ma8888b.
-      \033[37md888888b.     \033[31m[techgnome]
-      \033[37m8P"YP"Y88     \033[31m[OS]: \033[39mDebian 8.6 jessie
-      \033[37m8|o||o|88     \033[31m[Kernel]: \033[39mx86_64 Linux 3.16.0-4-amd64
-      \033[37m8'    .88     \033[31m[WM]: \033[39mOpenBox
+      \033[37md888888b.     \033[32m[{}]
+      \033[37m8P"YP"Y88     \033[32m[OS]: \033[39mDebian 8.6 jessie
+      \033[37m8|o||o|88     \033[32m[Kernel]: \033[39m {}
+      \033[37m8'    .88     \033[32m[WM]:\033[39m OpenBox
       \033[37m8`._.' Y8.
      \033[37md/      `8b.
     \033[37mdP   .    Y8b.
@@ -40,10 +47,10 @@ linux = '''
 \033[37m._/"Yaa_:   .| 88P|
 \033[37m\    YP"    `| 8P  `.
 \033[37m/     \.___.d|    .'
-\033[37m`--..__)8888P`._.'
+\033[37m`--..__)8888P`._.' \033[39m
 
 
-'''
+'''.format(getpass.getuser(),platform.release())
 
 
 
